20a21f09e068ac60c917d397f1ab49d1500838f4
mastergs95 <sousapro95@gmail.com>
2022-01-20 15:44:34 +0000

    Add project files.

---
+++ PAP3/Models/Clientes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAP3.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength = 11)]
+        [Display(Name = "Email")]
+        public int Email { get; set; }
+
+        [StringLength(3)]
+        [Display(Name = "Idioma")]
+        public string Idioma { get; set; }
+
+        [StringLength(14, MinimumLength = 12)]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        public string Imagem { get; set; }
+
+
+
+    }
+}
---
+++ PAP3/Models/Funcionarios.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAP3.Models
+{
+    [Table("Funcionarios")]
+    public class Funcionario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength = 11)]
+        [Display(Name = "Email")]
+        public int Email { get; set; }
+
+        
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [StringLength(9, MinimumLength = 8)]
+        [Display(Name = "Género")]
+        public String Genero { get; set; }
+
+        [StringLength(14, MinimumLength = 12)]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        public string Imagem { get; set; }
+
+
+
+    }
+}
